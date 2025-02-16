aecf9edd740bfc56c1cb82ce4621fea443f1a6f3
Daniel Palma <danieljpm554@gmail.com>
2025-02-15 21:59:03 -0500

    Extract to components

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,49 @@
 import "./App.css";
 import { useState } from "react";
+import confetti from "canvas-confetti";
+import { Square } from "./components/Square.jsx";
+import { TURNS } from "./constants.js";
+import { checkWinnerFrom, checkEndGame } from "./logic/board.js";
+import { WinnerModal } from "./components/WinnerModal.jsx";
 
-const TURNS = {
-  X: "x",
-  O: "o",
-};
-
-const Square = ({ children, isSelected, updateBoard, index }) => {
-  const className = `square ${isSelected ? "is-selected" : ""}`;
-  return <div className="square">{children}</div>;
-};
 function App() {
   const [board, setBoard] = useState(Array(9).fill(null));
   const [turn, setTurn] = useState(TURNS.X);
+  const [winner, setWinner] = useState(null); // null es que no hay ganador y false es que hay empate
+
+  const resetGame = () => {
+    setBoard(Array(9).fill(null));
+    setWinner(null);
+    setTurn(TURNS.X);
+  };
+
+  const updateBoard = (index) => {
+    // No se puede jugar en una celda ocupada
+    if (board[index] || winner) return;
+    // Actualizar el tablero
+    const newBoard = [...board];
+    newBoard[index] = turn;
+    setBoard(newBoard);
+    // Cambiar el turno
+    const newTurn = turn === TURNS.X ? TURNS.O : TURNS.X;
+    setTurn(newTurn);
+    // Revisar si hay un ganador
+    const newWinner = checkWinnerFrom(newBoard);
+    if (newWinner) {
+      confetti;
+      setWinner(newWinner); // actualiza el estado
+    } else if (checkEndGame(newBoard)) {
+      setWinner(false); // Empate
+    }
+  };
   return (
     <main className="board">
-      <h1>Hello World</h1>
+      <h1>Tic Tac Toe</h1>
+      <button onClick={resetGame}>Reiniciar</button>
       <section className="game">
-        {board.map((_, index) => (
-          <Square key={index} index={index}>
-            {index}
+        {board.map((__, index) => (
+          <Square key={index} index={index} updateBoard={updateBoard}>
+            {board[index]}
           </Square>
         ))}
       </section>
@@ -27,6 +51,7 @@ function App() {
         <Square isSelected={turn === TURNS.X}>{TURNS.X}</Square>
         <Square isSelected={turn === TURNS.O}>{TURNS.O}</Square>
       </section>
+      <WinnerModal winner={winner} resetGame={resetGame} />
     </main>
   );
 }
---
+++ src/components/WinnerModal.jsx
@@ -0,0 +1,18 @@
+import { Square } from "./Square.jsx";
+export function WinnerModal({ winner, resetGame }) {
+  if (winner === null) return null;
+  const winnerText = winner === false ? "Empate" : "Ganó";
+  return (
+    <div className="winner">
+      <div className="text">
+        <h2>{winnerText}</h2>
+        <header className="win">
+          {winner && <Square isSelected={true}>{winner}</Square>}
+        </header>
+        <footer>
+          <button onClick={resetGame}>Reiniciar</button>
+        </footer>
+      </div>
+    </div>
+  );
+}
